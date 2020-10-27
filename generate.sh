31c40477f2d68be7ab04a11352a6b5dd37ed08b4
Deven Phillips <deven.phillips@redhat.com>
2020-10-27 14:19:36 -0400

    Updated generator config

--- generate.sh
+++ generate.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 openapi-generator generate -g aspnetcore \
-  -t /home/dphillips/Documents/RedHat/Workspace/CNAD_Enablement/openapi-generator-aspnetcore3-templates \
+  --template-dir ${PWD}/openapi-generator-aspnetcore3-templates/ \
   --additional-properties aspnetCoreVersion=3.1 \
   --additional-properties classModifier=abstract \
   --additional-properties operationModifier=abstract \
-  --additional-properties packageName=Com.RedHat.TodoList \
+  --additional-properties packageName=RedHat.TodoList \
   --additional-properties packageTitle=TodoList \
-  --additional-properties useFrameworkReference=true \
-  --additional-properties returnICollection=true \
+  --additional-properties returnICollection=false \
   --additional-properties operationResultTask=true \
   -i todo_openapi.yaml \
-  -o $(pwd) 
-
-##  -t $(pwd)/../openapi-generator-aspnetcore3-templates \
+  -o ${PWD}
